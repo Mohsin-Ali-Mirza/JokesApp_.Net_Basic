be702d198aa5a24a8f5399a950fae55daac84d18
Mohsin Ali Mirza <mohsinalimirxa@gmail.com>
2023-11-16 17:26:53 +0500

    basic crud

---
+++ JokesApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesApp2.Models;
+
+namespace JokesApp2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesApp2.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ JokesApp2/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesApp2.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
